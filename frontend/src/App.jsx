188102c0bc0cad8339464bb699baa2f69cb17c77
Teeruth Ieowsakulrat <teeruth2546@gmail.com>
2024-09-09 21:38:00 +0700

    update shop profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,8 @@ import ClientProfilePostPage from "./pages/ClientProfilePostPage"
 import ShopProfileEditPage from "./pages/ShopProfileEditPage"
 import ShopProfileOrderHistoryPage from "./pages/ShopProfileOrderHistoryPage"
 import ShopProfileChangePsPage from "./pages/ShopProfileChangePSpage"
+import ShopProfileBankPage from "./pages/ShopProfileBankPage"
+import ShopProfileReviewHistoryPage from "./pages/ShopProfileReviewHistoryPage"
 
 function App() {
   return (
@@ -34,6 +36,8 @@ function App() {
         <Route path="/shop/profile" element={<ShopProfileEditPage/>} />
         <Route path="/shop/profile/order" element={<ShopProfileOrderHistoryPage/>} />
         <Route path="/shop/profile/password" element={<ShopProfileChangePsPage/>} />
+        <Route path="/shop/profile/bank" element={<ShopProfileBankPage/>} />
+        <Route path="/shop/profile/reviews" element={<ShopProfileReviewHistoryPage/>} />
 
 
       </Routes>
