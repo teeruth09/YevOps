2e9456e7e0a0e5c0e10ec4e82fdd0e7b074fe788
Nutchanon Mongkonvilas <nutchanon.m@lakeikeda.com>
2024-09-11 14:47:21 +0700

    feat: created shop detail cards

--- frontend/src/pages/Viewshoppage.jsx
+++ frontend/src/pages/Viewshoppage.jsx
@@ -1,7 +1,7 @@
-// import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar'
 import { FaStar } from 'react-icons/fa6'
 
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 import {
   Carousel,
@@ -10,10 +10,11 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel'
+import { Progress } from '@/components/ui/progress'
 
 const Viewshoppage = () => {
-  const [mainCarouselApi, setMainCarouselApi] = useState()
   const [carouselApi, setCarouselApi] = useState()
+  const [mainCarouselIndex, setMainCarouselIndex] = useState(0)
 
   const mockImages = [
     'mock-products/hitman-suit.png',
@@ -23,9 +24,27 @@ const Viewshoppage = () => {
     'profile.png',
   ]
 
+  // The carosel with 3 images can only go next if the current image is not the first or the one before last
+  const carouselScrollNext = useCallback(() => {
+    setMainCarouselIndex((prev) => prev + 1)
+
+    if (carouselApi && !(mainCarouselIndex < 1)) {
+      console.log('next updated')
+      carouselApi.scrollNext()
+    }
+  }, [carouselApi, mainCarouselIndex])
+
+  // The carosel with 3 images can only go prev if the current image is not the last or the one after the first
+  const carouselScrollPrev = useCallback(() => {
+    setMainCarouselIndex((prev) => prev - 1)
+
+    if (carouselApi && !(mainCarouselIndex >= mockImages.length - 1))
+      carouselApi.scrollPrev()
+  }, [carouselApi, mainCarouselIndex, mockImages.length])
+
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
 
       <div className='flex flex-col w-full p-6 gap-4'>
         <p className='text-sm'>View Shop &gt; Shop</p>
@@ -36,7 +55,7 @@ const Viewshoppage = () => {
               <img
                 src='profile.png'
                 alt='profile'
-                className='w-8 h-8 rounded-full'
+                className='w-12 h-12 rounded-full'
               />
               <p className='text-xl font-bold mx-2'>Nai_mana dotshop</p>
               <FaStar />
@@ -44,7 +63,7 @@ const Viewshoppage = () => {
             </div>
 
             <div className='flex items-center justify-center bg-gray-300 w-full aspect-[4/3]'>
-              <Carousel setApi={setMainCarouselApi} className='w-3/4'>
+              <Carousel className='w-3/4'>
                 <CarouselContent>
                   {mockImages.map((url) => (
                     <CarouselItem
@@ -59,24 +78,10 @@ const Viewshoppage = () => {
                     </CarouselItem>
                   ))}
                 </CarouselContent>
-                <div
-                  onClick={() => {
-                    if (carouselApi) {
-                      carouselApi.scrollPrev()
-                    }
-                  }}
-                >
-                  <CarouselPrevious />
-                </div>
-                <div
-                  onClick={() => {
-                    if (carouselApi) {
-                      carouselApi.scrollNext()
-                    }
-                  }}
-                >
-                  <CarouselNext />
-                </div>
+
+                <CarouselPrevious onClick={carouselScrollPrev} />
+
+                <CarouselNext onClick={carouselScrollNext} />
               </Carousel>
             </div>
 
@@ -86,13 +91,115 @@ const Viewshoppage = () => {
                   {mockImages.map((url) => (
                     <CarouselItem
                       key={url}
-                      className='flex justify-center items-center basis-1/3 aspect-[4/3] bg-gray-300 pl-0'
+                      className='flex justify-center items-center basis-1/3 bg-gray-300 pl-0'
                     >
-                      <img src={url} alt='product' className='object-cover' />
+                      <img
+                        src={url}
+                        alt='product'
+                        className='w-full aspect-[4/3] object-cover'
+                      />
                     </CarouselItem>
                   ))}
                 </CarouselContent>
               </Carousel>
+            </div>
+
+            <div className='mt-8 flex flex-col gap-2 border-2 p-6 rounded shadow'>
+              <p className='text-2xl'>About us</p>
+              <div className='w-full h-0.5 bg-gray-300' />
+
+              <div className='grid grid-cols-3 gap-2'>
+                <div className='flex flex-col col-span-2 gap-2'>
+                  <p>Address</p>
+                  <p>
+                    เลขที่ 352/29 แขวง คลองต้นนุ่น เขตลาดกระบัง กรุงเทพมหานคร
+                    10520
+                  </p>
+
+                  <p>Contact us</p>
+                  <p>0945552867</p>
+                </div>
+
+                <div className='flex flex-col gap-2'>
+                  <p>Member since</p>
+                  <p>กันยายน 2024</p>
+                </div>
+              </div>
+
+              <p className='text-2xl mt-8'>Description</p>
+              <div className='w-full h-0.5 bg-gray-300' />
+
+              <div className='grid grid-cols-3 gap-2'>
+                <div className='flex flex-col col-span-2'>
+                  <p>
+                    สวัสดีครับพวกเรา Nai_mana_dotshop รับตัดชุด Cosplay ทุกชนิด
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className='mt-8 flex flex-col border-2 p-6 rounded shadow'>
+              <p className='text-2xl'>Reviews</p>
+              <div className='w-full h-0.5 bg-gray-300' />
+
+              <p className='my-4'>37 reviews for this dressmaker</p>
+
+              <div className='w-1/2 flex gap-2 items-center'>
+                <p className='whitespace-nowrap pr-2'>5 Stars</p>
+                <Progress value={38} className='mt-1' />
+                <p>(37)</p>
+              </div>
+              <div className='w-1/2 flex gap-2 items-center'>
+                <p className='whitespace-nowrap pr-2'>4 Stars</p>
+                <Progress value={38} className='mt-1' />
+                <p>(37)</p>
+              </div>
+              <div className='w-1/2 flex gap-2 items-center'>
+                <p className='whitespace-nowrap pr-2'>3 Stars</p>
+                <Progress value={38} className='mt-1' />
+                <p>(37)</p>
+              </div>
+              <div className='w-1/2 flex gap-2 items-center'>
+                <p className='whitespace-nowrap pr-2'>2 Stars</p>
+                <Progress value={38} className='mt-1' />
+                <p>(37)</p>
+              </div>
+              <div className='w-1/2 flex gap-2 items-center'>
+                <p className='whitespace-nowrap pr-2'>1 Stars</p>
+                <Progress value={38} className='mt-1' />
+                <p>(37)</p>
+              </div>
+            </div>
+
+            <div className='mt-8 flex flex-col gap-2 border-2 p-6 rounded shadow'>
+              <div className='flex items-center gap-2'>
+                <img
+                  src={mockImages[4]}
+                  alt='profile'
+                  className='h-12 w-12 rounded-full'
+                />
+
+                <div className='flex flex-col'>
+                  <p>สมบูรณายาสิทธิราช</p>
+                  <div className='flex items-center gap-2'>
+                    <div className='flex items-center'>
+                      <FaStar />
+                      <FaStar />
+                      <FaStar />
+                      <FaStar />
+                      <FaStar />
+                    </div>
+                    <p>5</p>
+                    <p>|</p>
+                    <p>15 Aug 2024</p>
+                  </div>
+                </div>
+              </div>
+              <div className='w-full h-0.5 bg-gray-300' />
+              <p>
+                สามารถตัดเย็บได้แบบว้าวซ่าจิงเกอเบล ทำยังไงก็ได้แต่ว่านะ
+                ร้านอาจจะของระยะเวลา ในการทำสักหลายวันเพราะว่าถูกสุด
+              </p>
             </div>
           </div>
         </div>
