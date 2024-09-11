ef34f7d7a9d91135b85a451de4d592dc147cf980
Hinoshii_38 <thapatw.bs@gmail.com>
2024-09-11 20:35:16 +0700

    Installed MUI, added VerifyShopPage (80%) updated shopcard and ShowShopAfterSearch page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ import ShopProfileOrderHistoryPage from "./pages/ShopProfileOrderHistoryPage"
 import ShopProfileChangePsPage from "./pages/ShopProfileChangePSpage"
 import ShopProfileBankPage from "./pages/ShopProfileBankPage"
 import ShopProfileReviewHistoryPage from "./pages/ShopProfileReviewHistoryPage"
+import Verifyshop from "./pages/VerifyShopPage" 
 
 function App() {
   return (
@@ -38,7 +39,7 @@ function App() {
         <Route path="/shop/profile/password" element={<ShopProfileChangePsPage/>} />
         <Route path="/shop/profile/bank" element={<ShopProfileBankPage/>} />
         <Route path="/shop/profile/reviews" element={<ShopProfileReviewHistoryPage/>} />
-
+        <Route path="/verifyshop" element={<Verifyshop/>} />
 
       </Routes>
    
