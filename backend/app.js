e3ec6b3175d283aed31dc3b75a1feca0fccd6860
Thanasak Songsri <Thanasuksongsriohm@gmail.com>
2024-09-15 12:19:44 +0700

    add order

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ require('./configs/database').connect()
 
 const express = require('express')
 const auth = require('./controllers/auth');
+const order = require('./controllers/order')
 const midauth = require('./middlewares/auth')
 
 
@@ -13,14 +14,13 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.send('Hello!'))
 
-
 app.post('/register', auth.register);
-
 
 app.post("/login", auth.login);
 
-app.post('/welcome', midauth, (req, res) => {
-    res.status(200).send("Welcome HACKER");
-})
+app.post("/createOrder", midauth, order.order);
+
+app.post("/updateStatus", midauth, order.statusOrder);
+
 
 module.exports = app
